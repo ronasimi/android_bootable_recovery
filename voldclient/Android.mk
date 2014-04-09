21875bccff43035088b82d62f2d897007dba7194
Ron Asimi <ron.asimi@gmail.com>
2014-04-09 08:59:14 -0400

    linaro fixes
    Change-Id: I11ef415ca83f1eecb2682440f9f36b246a60ce1e

--- voldclient/Android.mk
+++ voldclient/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvoldclient
 LOCAL_SRC_FILES := commands.c dispatcher.c event_loop.c
-LOCAL_CFLAGS := -DMINIVOLD -Werror
+LOCAL_CFLAGS := -DMINIVOLD -Werror -Wno-error=unused-parameter
 LOCAL_C_INCLUDES :=         	\
     bootable/recovery       	\
     system/core/fs_mgr/include	\
